67782f7a31ac82630acaeeb1683a8a0c6f3861a9
Sava Mihajlovic 18784 <sava.mihajlovic@elfak.rs>
2024-04-29 01:22:06 +0200

    Spinner

--- Aplikacija/frontend/react_app/src/App.jsx
+++ Aplikacija/frontend/react_app/src/App.jsx
@@ -3,30 +3,38 @@ import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {Home,Login,Register,About} from './pages'
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
 
 const App = () => {
-
   const current_theme = localStorage.getItem('current_theme');
-  const[theme , setTheme] = useState(current_theme ? current_theme: 'light');
+  const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      localStorage.setItem('current_theme', theme);
-  },[theme])
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className={`container ${theme}`}>
       <BrowserRouter>
         <Navbar theme={theme} setTheme={setTheme} />
-        <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/about' element={<About />}/>
-      </Routes>
-        <Footer theme={theme} />
+        <div className="content-wrapper">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <Footer theme={theme} />
+        </div>
       </BrowserRouter>
+      {loading && <div className="loading-container"><LoadingSpinner /></div>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Aplikacija/frontend/react_app/src/pages/Home.jsx
+++ Aplikacija/frontend/react_app/src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import videoBG from '../assets/videoBG.mp4'
 
 export const Home = () => {
   return (
     <div className='main'>
-      <video src={videoBG} autoPlay loop muted/>
-    </div>
 
-    //<div>Home</div>
+      <video src={videoBG} autoPlay loop muted className='fullscreen-video'/>
+      <div className='overlay'></div>
+
+      <div className='content'>
+        <h1>Welcome</h1>
+        <p>To WAZAP.</p>
+        <Link to="/about" className="about-button">About us</Link>
+      </div>
+    </div>
   )
 }
